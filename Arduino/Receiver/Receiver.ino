08eba811844afe45adb96d041f712c2c89eb94b1
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2023-08-05 21:19:45 +0530

    Receiver

---
+++ Arduino/Receiver/Receiver.ino
@@ -0,0 +1,26 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // NRF24L01 pins (CE, CSN)
+const byte address[6] = "00001";
+
+
+void setup() {
+  while (!Serial);
+    Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+void loop() {
+ // Receive data packet
+  if(radio.available())
+  {
+  char data[2]={0};
+  radio.read(&data, sizeof(data));
+  Serial.println(data);
+  }
+}
