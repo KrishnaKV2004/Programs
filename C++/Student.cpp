8bcd52b2afef211984bf36c5ed6f3b51cdaa8716
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2023-09-06 15:40:45 +0530

    Commit

---
+++ C++/Student.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class student
+{
+    private :
+        string name;
+        string course;
+        int roll;
+        string sub1;
+        string sub2;
+        string sub3;
+        double perc;
+};
+
+int main()
+{
+
+    return 0;
+}
