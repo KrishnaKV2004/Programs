9e28fe7e201e48e172a6b3b723a9c221b6466c8e
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2023-09-18 00:01:59 +0530

    Singly Linked List Update

--- C/DSA/Singly_LL.c
+++ C/DSA/Singly_LL.c
@@ -37,161 +37,178 @@ int main()
     //  Menu For Operations -->
 
     printf("\n# -------- Linked List -------- #\n");
-    printf("\nOperations ---->\n");
-    printf("\n[I] To Insert Node");
-    printf("\n[D] To Delete Node");
-    printf("\n[V] To View Data");
-    printf("\n\nChoose Operation ----> ");
-    sscanf(" %c", &choice);
 
-    //  Checking For Operation Cases -->
+    menu :
 
-    switch (choice)
-    {
-        //  To Insert -->
+        printf("\nOperations ---->\n");
+        printf("\n[I] To Insert Node");
+        printf("\n[D] To Delete Node");
+        printf("\n[V] To View Data");
+        printf("\n\nChoose Operation ----> ");
+        scanf(" %c", &choice);
 
-        case 'I' :
-        case 'i' :
+        //  Checking For Operation Cases -->
 
-            //  Checking Further Possibilities -->
+        switch (choice)
+        {
+            //  To Insert -->
 
-            printf("\nChoose Place To Insert ---->\n");
-            printf("\n[F] To Insert At First Position");
-            printf("\n[L] To Insert At Last Position");
-            printf("\n[C] To Insert At Custom Position");
-            printf("\nChoose Operation --> ");
-            scanf(" %c", &pos);
+            case 'I' :
+            case 'i' :
 
-            switch (pos)
-            {
-                //  To Insert At First Position -->
+                //  Checking Further Possibilities -->
 
-                case 'F' :
-                case 'f' :
-                    
-                    //  Function To Insert Node At First Position
-                    head = f_ins(head);
-                    break;
-            
-                //  To Insert At Last Position -->
+                printf("\nChoose Place To Insert ---->\n");
+                printf("\n[F] To Insert At First Position");
+                printf("\n[L] To Insert At Last Position");
+                printf("\n[C] To Insert At Custom Position");
+                printf("\n\nChoose Operation --> ");
+                scanf(" %c", &pos);
 
-                case 'L' :
-                case 'l' :
+                switch (pos)
+                {
+                    //  To Insert At First Position -->
 
-                    //  Function To Insert Node At Last Position
-                    head = l_ins(head);
-                    break;
+                    case 'F' :
+                    case 'f' :
 
-                //  To Insert At Custom Position -->
+                        //  Function To Insert Node At First Position
+                        head = f_ins(head);
+                        break;
 
-                case 'C' :
-                case 'c' :
+                    //  To Insert At Last Position -->
 
-                    //  Function To Insert Node At Specific Position
-                    head = c_ins(head);
-                    break;
+                    case 'L' :
+                    case 'l' :
 
-                default:
+                        //  Function To Insert Node At Last Position
+                        head = l_ins(head);
+                        break;
 
-                    //  Message For Invalid Input Choice
-                    printf("\nInvalid Operation !");
-                    break;
-            }
-            break;
+                    //  To Insert At Custom Position -->
 
-        //  To Delete -->
+                    case 'C' :
+                    case 'c' :
 
-        case 'D' :
-        case 'd' :
+                        //  Function To Insert Node At Specific Position
+                        head = c_ins(head);
+                        break;
 
-            //  Checking Further Possibilities -->
+                    default:
 
-            printf("\nChoose Place To Delete ---->\n");
-            printf("\n[F] To Delete First Node");
-            printf("\n[L] To Delete Last Node");
-            printf("\n[C] To Delete Custom Node");
-            printf("\nChoose Operation --> ");
-            scanf(" %c", &pos);
+                        //  Message For Invalid Input Choice
+                        printf("\nInvalid Operation !");
+                        break;
+                }
+                break;
 
-            switch (pos)
-            {
-                //  To Delete First Node -->
+            //  To Delete -->
 
-                case 'F' :
-                case 'f' :
-                    
-                    //  Function To Delete First Node
-                    head = f_del(head);
-                    break;
-            
-                case 'L' :
-                case 'l' :
+            case 'D' :
+            case 'd' :
 
-                    //  Function To Delete Last Node
-                    head = l_del(head);
-                    break;
+                //  Checking Further Possibilities -->
 
-                case 'C' :
-                case 'c' :
+                printf("\nChoose Place To Delete ---->\n");
+                printf("\n[F] To Delete First Node");
+                printf("\n[L] To Delete Last Node");
+                printf("\n[C] To Delete Custom Node");
+                printf("\n\nChoose Operation --> ");
+                scanf(" %c", &pos);
 
-                    //  Function To Delete Custom Node
-                    head = c_del(head);
-                    break;
+                switch (pos)
+                {
+                    //  To Delete First Node -->
 
-                default:
+                    case 'F' :
+                    case 'f' :
 
-                    //  Message For Invalid Input Choice
-                    printf("\nInvalid Operation !");
-                    break;
-            }
-            break;
+                        //  Function To Delete First Node
+                        head = f_del(head);
+                        break;
 
-        //  To View Data Of Linked List -->
+                    case 'L' :
+                    case 'l' :
 
-        case 'V' :
-        case 'v' :
+                        //  Function To Delete Last Node
+                        head = l_del(head);
+                        break;
 
-            //  Checking For Further Viewing Possibilities -->
+                    case 'C' :
+                    case 'c' :
 
-            printf("\nChoose Operation To View ---->\n");
-            printf("\n[A] To View All Data In Linked List");
-            printf("\n[N] To View Data Of Specific Node");
-            printf("\nChoose Operation --> ");
-            scanf(" %c", &pos);
+                        //  Function To Delete Custom Node
+                        head = c_del(head);
+                        break;
 
-            switch (pos)
-            {
-                //  To View Entire Linked List -->
+                    default:
 
-                case 'A' :
-                case 'a' :
-                    
-                    //  Function To view Entire Linked List
-                    f_view(head);
-                    break;
+                        //  Message For Invalid Input Choice
+                        printf("\nInvalid Operation !");
+                        break;
+                }
+                break;
 
-                //  To View Data At A Specific Node
-                case 'N' :
-                case 'n' :
+            //  To View Data Of Linked List -->
 
-                    //  Function To View Data At Specific Node
-                    c_view(head);
-                    break;
+            case 'V' :
+            case 'v' :
 
-                default :
+                //  Checking For Further Viewing Possibilities -->
 
-                    //  Message For Invalid Input Choice
-                    printf("\nInvalid Operation !");
-                    break;
-            }    
-            break;
+                printf("\nChoose Operation To View ---->\n");
+                printf("\n[A] To View All Data In Linked List");
+                printf("\n[N] To View Data Of Specific Node");
+                printf("\n\nChoose Operation --> ");
+                scanf(" %c", &pos);
 
-        default :
+                switch (pos)
+                {
+                    //  To View Entire Linked List -->
 
-            //  Message For Invalid Input Choice
-            printf("\nInvalid Operation !");
-            break;
-    }
+                    case 'A' :
+                    case 'a' :
+
+                        //  Function To view Entire Linked List
+                        f_view(head);
+                        break;
+
+                    //  To View Data At A Specific Node
+                    case 'N' :
+                    case 'n' :
+
+                        //  Function To View Data At Specific Node
+                        c_view(head);
+                        break;
+
+                    default :
+
+                        //  Message For Invalid Input Choice
+                        printf("\nInvalid Operation !");
+                        break;
+                }    
+                break;
+
+            default :
+
+                //  Message For Invalid Input Choice
+                printf("\nInvalid Operation !");
+                break;
+        }
+
+        //  To Continue Operations -->
+
+        printf("\nContinue Operation ? [Y/N] --> ");
+        scanf(" %c", &con);
+
+        if (con=='Y' || con=='y')
+        {
+            goto menu;  //  If True Goto Menu
+        }
+        else
+        {
+            exit(0);    //  Else Exit
+        }
 
     return 0;
 }
@@ -201,17 +218,20 @@ int main()
 nd* f_ins(nd *head)
 {
     int val;
-    nd *new_node = (nd*) malloc(sizeof(nd));
+    nd *new_node = (nd *)malloc(sizeof(nd));
 
-    if (*new_node == NULL)
+    if (new_node == NULL)
     {
+        //  If Memory Not Allocated
         printf("\nSorry ! Memory Not Allocated");
         return head;
     }
 
+    //  Input Value To Store
     printf("\nEnter Data --> ");
     scanf("%d", &val);
 
+    //  Joining New Node And Updating Head
     new_node->data = val;
     new_node->next = head;
     head = new_node;
@@ -248,9 +268,12 @@ void f_view(nd *head)
 {
     while (head != NULL)
     {
+        //  Traversing Linked List And Printing Data
         printf("\nData --> %d", head->data);
-        head = head->next;
+        head = head->next;  //  Moving To Next Node
     }
+
+    printf("\n");
 }
 
 void c_view(nd *head)
