670bbd97a651204ec5d5d35b23f913327916dd5b
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2023-09-18 23:59:35 +0530

    Commit

--- C/DSA/Singly_LL.c
+++ C/DSA/Singly_LL.c
@@ -184,7 +184,7 @@ int main()
                     default :
 
                         //  Message For Invalid Input Choice
-                        printf("\nInvalid Operation !");
+                        printf("\nInvalid Operation !\n");
                         break;
                 }    
                 break;
