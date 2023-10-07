4def419bc057639779db65bc591b02c82fa76ae8
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2023-10-07 06:53:22 +0530

    Added Basic Python

---
+++ Python/Practise/Basic/Loop.py
@@ -0,0 +1,15 @@
+# While Loop -->
+
+i = 1
+
+while i < 11 :
+    print(i)
+    i += 1
+    
+print()
+    
+# For Loop -->
+
+for i in range(1,4) :
+    for j in range(1,6) :
+        print(i,j)
---
+++ Python/Practise/Basic/String.py
@@ -0,0 +1,9 @@
+str = "Hello"
+
+print()
+print(str[:])
+print(str[::-1])
+print(str[-5:-1])
+print(str.upper())
+print(str.lower())
+print(len(str))
