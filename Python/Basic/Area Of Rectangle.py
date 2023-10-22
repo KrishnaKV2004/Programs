3d2589d67e560747c46501edc2952ac88b7fe4e5
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2023-10-22 22:35:40 +0530

    Deleted Files

--- Python/Basic/Area Of Rectangle.py
+++
@@ -1,6 +0,0 @@
-l=float(input("Enter Length "))
-b=float(input("Enter Breadth "))
-p=2*(l+b)
-a=l*b
-print("Perimeter",p)
-print("Area : ",a)
--- Python/Basic/BMI.py
+++
@@ -1,4 +0,0 @@
-w=float(input("Enter Weight in Kg :"))
-h=float(input("Enter Heigth in m :"))
-b=w/(h**2)
-print("BMI :",b)
