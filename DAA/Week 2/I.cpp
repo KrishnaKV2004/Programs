be5fa6138dfeefbddbb8e024dabe113cdf38b6f0
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2024-02-07 21:30:26 +0530

    Added II.cpp

--- DAA/Week 2/I.cpp
+++ DAA/Week 2/I.cpp
@@ -2,17 +2,33 @@
 
 using namespace std;
 
-int binarySearch(int arr[], int low, int high, int key)
+int binarySearch(int arr[], int low, int high, int key, int& count_duplicates)
 {
-    while (low<=high)
+    while (low <= high)
     {
         int mid = low + (high - low) / 2;
 
         if (key == arr[mid])
         {
+            count_duplicates = 1;
+
+            int left = mid - 1;
+            while (left >= low && arr[left] == key)
+            {
+                count_duplicates++;
+                left--;
+            }
+
+            int right = mid + 1;
+            while (right <= high && arr[right] == key)
+            {
+                count_duplicates++;
+                right++;
+            }
+
             return mid;
         }
-        if (key > array[mid])
+        else if (key > arr[mid])
         {
             low = mid + 1;
         }
@@ -29,33 +45,35 @@ int main()
 {
     int n;
 
-    cout << "Enter The Size Of Array --> ";
+    cout << "\nEnter The Size Of Array --> ";
     cin >> n;
 
     int Array[n];
 
-    cout << "Enter Elements In Array In Sorted Order --> " << endl;
+    cout << "\nEnter Elements In Array In Sorted Order --> \n" << endl;
 
-    for (int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout << "Element " << i+1 << ": ";
+        cout << "Element " << i + 1 << ": ";
         cin >> Array[i];
     }
 
     int key;
 
-    cout << "Enter Key Element To Search --> ";
+    cout << "\nEnter Key Element To Search --> ";
     cin >> key;
 
-    int result = binarySearch(Array, 0, n - 1, key);
+    int count_duplicates = 0;
+    int result = binarySearch(Array, 0, n - 1, key, count_duplicates);
 
     if (result == -1)
     {
-        cout << "Element Not Found !" << endl;
+        cout << "\nElement Not Found !" << endl;
     }
     else
     {
-        cout << "Element Found At Index " << result << endl;
+        cout << "\nElement Found At Index " << result << endl;
+        cout << "\nNumber of Duplicates: " << count_duplicates - 1 << endl;
     }
 
     return 0;
